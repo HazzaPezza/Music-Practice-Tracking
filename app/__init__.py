d0b0d150783bd21ab3cb04d6871c4371a280439c
Harry Perriton <hjperriton@gmail.com>
2026-02-07 11:45:21 +0000

    Adding some additional blank pages and routes to them.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+# Instantiate flask app from environment variable.
 app = Flask(__name__)
 
+# Import routes, imported here to avoid circular imports.
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,19 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+  """
+  This is just a return function for the index route. Decorators above
+  provide the URLs that this function is mapped to through app.route().
+
+  :return: Returns render_template response which renders index.html
+  """
   return render_template('index.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/sign-up')
+def sign_up():
+  return render_template('sign-up.html')
+  
