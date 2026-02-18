d5bbe58f2ce36e9dc4571f94318b6633928329a1
Harry Perriton <hjperriton@gmail.com>
2026-02-18 14:21:35 +0000

    Finished an initial version of sign-up logic. No styling or other work on sign-up page.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+
+def create_app():
+  app = Flask(__name__)
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+  db.init_app(app)
+
+  with app.app_context():
+    from .routes import main_bp
+    app.register_blueprint(main_bp)
+
+    from app.models.track import User
+    db.create_all()
+
+  return app
 
 # Instantiate flask app from environment variable.
-app = Flask(__name__)
+app = create_app()
 
-# Import routes, imported here to avoid circular imports.
-from app import routes
+  
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
-from flask import render_template
-from app import app
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash
+from app import db
+from app.models.track import User
+main_bp = Blueprint('main', __name__)
 
 # ----- Index Routes and API Endpoints -----
-@app.route('/')
-@app.route('/index')
+@main_bp.route('/')
+@main_bp.route('/index')
 def index():
   """
   This is just a return function for the index route. Decorators above
@@ -16,13 +19,32 @@ def index():
 
 # ----- Sign-up Routes and API Endpoints -----
 # Going to start with sign up and post operations to DB.
-@app.route('/sign-up')
+@main_bp.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-  return render_template('sign-up.html')
+  if request.method == 'POST':
+    print(f"ALL FORM DATA: {request.form}")
+
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print("Username: {}, Email: {}, Password: {}".format(username, email, password))
+
+    if username and email and password:
+      hashed_pw = generate_password_hash(password, method='scrypt')
+
+      new_user = User(username=username, email=email, password_hash=hashed_pw)
+      db.session.add(new_user)
+      db.session.commit()
+      
+      return redirect(url_for('main.sign_up'))
+  
+  users = User.query.all()
+  return render_template('sign-up.html', users=users)
 
 
 # ----- Login Routes and API Endpoints -----
 # Second gonna do login so that I know what I'm doing with that.
-@app.route('/login')
+@main_bp.route('/login')
 def login():
   return render_template('login.html')
